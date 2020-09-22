9c5df142f8ca811b75888a0891fa033af11ecfd4
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-09-22 20:31:26 -0300

    Create file_helpers.py

---
+++ file_helpers.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+
+def adicionar_imagens(selection, imagens=None):
+    if imagens is None:
+	    imagens = []
+
+    if selection == None:
+        print("Seleção cancelada.")
+    else:
+        dir_path = selection.getAbsolutePath()
+        print("Pasta selecionada: " + dir_path)
+        for file_name, file_path in lista_imagens(dir_path):
+            img = loadImage(file_path)
+            img_name = file_name.split('.')[0]
+            print("imagem " + img_name + " carregada.")
+            imagens.append((img_name, img))
+        print('Número de imagens: ' + str(len(imagens)))
+    return imagens
+
+def lista_imagens(dir=None):
+    """
+    Devolve uma a lista de tuplas com os nomes dos arquivos de imagem e os caminhos
+    completos para cada uma das images na pasta `dir` ou na pasta /data/ do sketch.
+    Requer a função imgext() para decidir quais extensões aceitar.
+    """
+    from os import listdir
+    from os.path import isfile, join
+    data_path = dir or sketchPath('data')
+    try:
+        f_list = [(f, join(data_path, f)) for f in listdir(data_path)
+                  if isfile(join(data_path, f)) and imgext(f)]
+    except Exception as e:
+        print("Erro ({0}): {1}".format(e.errno, e.strerror))
+        return []
+    return f_list
+
+def imgext(file_name):
+    ext = file_name.split('.')[-1]
+    # extensões dos formatos de imagem que o Processing aceita!
+    valid_ext = ('jpg',
+                 'png',
+                 'jpeg',
+                 'gif',
+                 'tif',
+                 'tga',
+                 )
+    return ext.lower() in valid_ext
