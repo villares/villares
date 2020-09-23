5ec181aab58f91ca770d43a5568f737a05d7ac3c
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-09-23 18:59:58 -0300

    brutal variable rename
    p1, p2 -> a, b

--- line_geometry.py
+++ line_geometry.py
@@ -1,155 +1,250 @@
 # -*- coding: UTF-8 -*-
 
-def is_poly_self_intersecting(poly_points):
-    ed = edges(poly_points)
-    intersect = False
-    for p1, p2 in ed[::-1]:
-        for p3, p4 in ed[2::]:
-            # test only non consecutive edges
-            if (p1 != p3) and (p2 != p3) and (p1 != p4):
-                if line_instersect(Line(p1, p2), Line(p3, p4)):
-                    intersect = True
-                    break
-    return intersect
-     
-def is_inside(x, y, poly_points):   
-    min_, max_ = min_max(poly_points)
-    if x < min_.x or y < min_.y or x > max_.x or y > max_.y:
-        return False
-    
-    a = PVector(x, min_.y)
-    b = PVector(x, max_.y)
-    v_lines = inter_lines(Line(a, b), poly_points)
-    if not v_lines:
-        return False
-        
-    a = PVector(min_.x, y)
-    b = PVector(max_.x, y)
-    h_lines = inter_lines(Line(a, b), poly_points)
-    if not h_lines:
-        return False
-                
-    for v in v_lines:
-        for h in h_lines:
-            if line_instersect(v, h):
-                return True   
-                         
-    return False
 
-
-def inter_lines(L, poly_points):
-    inter_points = []
-    for p1, p2 in edges(poly_points):
-        inter = line_instersect(Line(p1, p2), L)
-        if inter:
-            inter_points.append(inter)
-    if  not inter_points:
-        return []
-    inter_lines = []
-    if len(inter_points) > 1:
-        inter_points.sort()
-        pairs = zip(inter_points[::2], inter_points[1::2])
-        for p1, p2 in pairs:
-            if p2:
-                inter_lines.append(Line(PVector(p1.x, p1.y),
-                                        PVector(p2.x, p2.y))) 
-    return inter_lines
-
-        
 class Line():
-    def __init__(self, p1, p2):
-        self.p1 = PVector(*p1)
-        self.p2 = PVector(*p2) 
-    
+
+    TOLERANCE = EPSILON
+
+    def __init__(self, a, b):
+        self.a = PVector(*a)
+        self.b = PVector(*b)
+
     def __getitem__(self, i):
-        return (self.p1, self.p2)[i]
-        
+        return (self.a, self.b)[i]
+
     def plot(self):
-        line(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
-    
+        line(self.a.x, self.a.y, self.b.x, self.b.y)
+
     draw = plot
-    
+
     def lerp(self, other, t):
-        p1 = PVector.lerp(self.p1, other.p1, t)
-        p2 = PVector.lerp(self.p2, other.p2, t)
-        return Line(p1, p2)
-    
-    def instersect(self, other):
-        return line_instersect(line_a, line_b)   
-    
-def line_instersect(line_a, line_b):     
+        a = PVector.lerp(self.a, other.a, t)
+        b = PVector.lerp(self.b, other.b, t)
+        return Line(a, b)
+
+    def intersect(self, other):
+        return line_instersect(self, other)
+
+    def point_over(self, x, y, TOLERANCE):
+        return point_over_line(x, y,
+                               self[0][0], self[0][1],
+                               self[1][0], self[1][1],
+                               TOLERANCE)
+
+    def point_colinear(self, x, y, TOLERANCE):
+        return points_are_colinear(x, y,
+                                   self[0][0], self[0][1],
+                                   self[1][0], self[1][1],
+                                   TOLERANCE)
+
+def line_instersect(line_a, line_b):
     """
     code adapted from Bernardo Fontes 
     https://github.com/berinhard/sketches/
     """
-    x1, y1 = line_a.p1.x, line_a.p1.y
-    x2, y2 = line_a.p2.x, line_a.p2.y
-    x3, y3 = line_b.p1.x, line_b.p1.y
-    x4, y4 = line_b.p2.x, line_b.p2.y
+    x1, y1 = line_a.a.x, line_a.a.y
+    x2, y2 = line_a.b.x, line_a.b.y
+    x3, y3 = line_b.a.x, line_b.a.y
+    x4, y4 = line_b.b.x, line_b.b.y
     try:
-        uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
-        uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+        uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / \
+            ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+        uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / \
+            ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
     except ZeroDivisionError:
         return
     if not(0 <= uA <= 1 and 0 <= uB <= 1):
         return
-    x = line_a.p1.x + uA * (line_a.p2.x - line_a.p1.x)
-    y = line_a.p1.y + uA * (line_a.p2.y - line_a.p1.y)
-        
+    x = line_a.a.x + uA * (line_a.b.x - line_a.a.x)
+    y = line_a.a.y + uA * (line_a.b.y - line_a.a.y)
     return PVector(x, y)
 
+def point_over_line(px, py, lax, lay, lbx, lby,
+                    tolerance=EPSILON):
+    """
+    Check if point is over line using the sum of
+    the distances from the point to the line ends
+    (the result has to be near equal for True).
+    """
+    ab = dist(lax, lay, lbx, lby)
+    pa = dist(lax, lay, px, py)
+    pb = dist(px, py, lbx, lby)
+    return (pa + pb) <= ab + tolerance / 100.0
+
+def points_are_colinear(ax, ay, bx, by, cx, cy,
+                        tolerance=EPSILON):
+    """
+    Test for colinearity by calculating the area
+    of a triangle formed by the 3 points.
+    """
+    area = triangle_area((ax, ay), (bx, by), (cx, cy))
+    return abs(area) < tolerance
+
+def triangle_area(p0, a, b):
+    area = (a[0] * (b[1] - p0[1]) +
+            b[0] * (p0[1] - a[1]) +
+            p0[0] * (a[1] - b[1]))
+    return area
+
+# class Poly():
+
+#     def __init__(iterable):
+#         self.__points = [p for p in iterable]
+    
+#     def __iter__(self):
+#         return iter(self.__points)
+    
+#     def plot(self):
+#         poly(self)
+        
+#     draw = poly        
+            
+
+def poly(points, holes=None, closed=True):
+    """
+    Aceita como pontos sequencias de tuplas, lista ou vetores com (x, y) ou (x, y, z).
+    Note que `holes` espera uma sequencias de sequencias ou uma única sequencia de
+    pontos. Por default faz um polígono fechado.
+    """
+
+    def depth(seq):
+        """
+        usada para checar se temos um furo ou vários
+        devolve 2 para um só furo, 3 para vários furos
+        """
+        if (isinstance(seq, list) or
+                isinstance(seq, tuple) or
+                isinstance(seq, PVector)):
+            return 1 + max(depth(item) for item in seq)
+        else:
+            return 0
+
+    beginShape()  # inicia o PShape
+    for p in points:
+        if len(p) == 2 or p[2] == 0:
+            vertex(p[0], p[1])
+        else:
+            vertex(*p)  # desempacota pontos em 3d
+    # tratamento dos furos, se houver           
+    holes = holes or []  # equivale a: holes if holes else []
+    if depth(holes) == 2:  # sequência única de pontos
+        holes = (holes,)     # envolve em um tupla
+    for hole in holes:  # para cada furo
+        beginContour()  # inicia o furo
+        for p in hole:
+            if len(p) == 2 or p[2] == 0:
+                vertex(p[0], p[1])
+            else:
+                vertex(*p)
+        endContour()  # final e um furo
+    # encerra o PShape
+    if closed:
+        endShape(CLOSE)
+    else:
+        endShape()
+
 def edges(poly_points):
-    return pairwise(poly_points) + [(poly_points[-1], poly_points[0])]   
+    return pairwise(poly_points) + [(poly_points[-1], poly_points[0])]
 
 def pairwise(iterable):
     import itertools
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
-    return zip(a, b) 
+    return zip(a, b)
 
 def edges_as_sets(poly_points):
     edge_set_pairs = set()
     for edge in edges(poly_points):
-       s = frozenset(edge)
-       edge_set_pairs.add(s)
+        s = frozenset(edge)
+        edge_set_pairs.add(s)
     return frozenset(edge_set_pairs)
 
 
 def min_max(points):
-        points = iter(points)
-        try:
-            p = points.next()
-            min_x, min_y = max_x, max_y = p.x, p.y
-        except StopIteration:
-            raise ValueError, "min_max requires at least one point"
-        for p in points:
-            if p.x < min_x:
-                min_x = p.x
-            elif p.x > max_x:
-                max_x = p.x
-            if p.y < min_y:
-                min_y = p.y
-            elif p.y > max_y:
-                max_y = p.y
-        return (PVector(min_x, min_y),
-                PVector(max_x, max_y))
-        
-def par_hatch(points, divisions, *sides):
-        vectors = [PVector(p.x, p.y) for p in points]
-        lines = []
-        if not sides: sides = [0]
-        for s in sides:
-            a, b = vectors[-1 + s], vectors[+0 + s]
-            d, c = vectors[-2 + s], vectors[-3 + s]
-            for i in range(1, divisions):
-                s0 = PVector.lerp(a, b, i/float(divisions))
-                s1 = PVector.lerp(d, c, i/float(divisions))
-                lines.append(Line(s0, s1)) 
-        return lines
+    points = iter(points)
+    try:
+        p = points.next()
+        min_x, min_y = max_x, max_y = p.x, p.y
+    except StopIteration:
+        raise ValueError, "min_max requires at least one point"
+    for p in points:
+        if p.x < min_x:
+            min_x = p.x
+        elif p.x > max_x:
+            max_x = p.x
+        if p.y < min_y:
+            min_y = p.y
+        elif p.y > max_y:
+            max_y = p.y
+    return (PVector(min_x, min_y),
+            PVector(max_x, max_y))
 
-def triangle_area(p0, p1, p2):
-    a = (p1[0] * (p2[1] - p0[1]) +
-         p2[0] * (p0[1] - p1[1]) +
-         p0[0] * (p1[1] - p2[1]))
-    return a
+def par_hatch(points, divisions, *sides):
+    vectors = [PVector(p.x, p.y) for p in points]
+    lines = []
+    if not sides:
+        sides = [0]
+    for s in sides:
+        a, b = vectors[-1 + s], vectors[+0 + s]
+        d, c = vectors[-2 + s], vectors[-3 + s]
+        for i in range(1, divisions):
+            s0 = PVector.lerp(a, b, i / float(divisions))
+            s1 = PVector.lerp(d, c, i / float(divisions))
+            lines.append(Line(s0, s1))
+    return lines
+
+def is_poly_self_intersecting(poly_points):
+    ed = edges(poly_points)
+    intersect = False
+    for a, b in ed[::-1]:
+        for c, d in ed[2::]:
+        # test only non consecutive edges
+            if (a != c) and (b != c) and (a != d):
+                if line_instersect(Line(a, b), Line(c, d)):
+                    intersect = True
+                    break
+    return intersect
+
+def point_inside_poly(x, y, poly_points):
+    min_, max_ = min_max(poly_points)
+    if x < min_.x or y < min_.y or x > max_.x or y > max_.y:
+        return False
+
+    a = PVector(x, min_.y)
+    b = PVector(x, max_.y)
+    v_lines = inter_lines(Line(a, b), poly_points)
+    if not v_lines:
+        return False
+
+    a = PVector(min_.x, y)
+    b = PVector(max_.x, y)
+    h_lines = inter_lines(Line(a, b), poly_points)
+    if not h_lines:
+        return False
+
+    for v in v_lines:
+        for h in h_lines:
+            if line_instersect(v, h):
+                return True
+
+    return False
+
+
+def inter_lines(L, poly_points):
+    inter_points = []
+    for a, b in edges(poly_points):
+        inter = line_instersect(Line(a, b), L)
+        if inter:
+            inter_points.append(inter)
+    if not inter_points:
+        return []
+    inter_lines = []
+    if len(inter_points) > 1:
+        inter_points.sort()
+        pairs = zip(inter_points[::2], inter_points[1::2])
+        for a, b in pairs:
+            if b:
+                inter_lines.append(Line(PVector(a.x, a.y),
+                                        PVector(b.x, b.y)))
+    return inter_lines
