8b5db5552c7ae689ef0bb76596fc472d863a5227
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-10-08 15:10:42 -0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,6 +12,7 @@
 # 2022_06_13 Added save_png_with_src() (for py5 only)
 # 2022_07_14 If on py5 imported mode, save_png_with_src now tries folder+'.py'
 # 2022_07_27 Added datetimestamp() & tweaks to save_png_with_src
+# 2022_08_03 Added get_arduino() based on PyFirmata (for py5 only)
 
 try:
     lerp
@@ -136,7 +137,7 @@ def datetimestamp(time_prefix='t', time_only=False, date_only=False):
 
 def save_png_with_src(output=None, *args, **kwargs):
     import py5
-    import PIL
+    import PIL.PngImagePlugin
     import glob
     from os.path import basename, join
     import __main__ as m
@@ -145,7 +146,7 @@ def save_png_with_src(output=None, *args, **kwargs):
         src_path = join(py5.sketch_path(), '*.py')
         src_file = glob.glob(str(src_path))[0]
         #src_file = join(file_path, basename(file_path) + '.py')
-
+    
     add_ts = kwargs.pop('timestamp', False) 
     add_dts = kwargs.pop('datetimestamp', True) 
     if add_ts:  # timestamp True overrides datetimestamp True
@@ -167,6 +168,16 @@ def save_png_with_src(output=None, *args, **kwargs):
     
     with open(src_file) as f:
         src = ''.join(f.read())
+        
+    other_files = kwargs.pop('files', '').split(',')
+    for file_name in other_files:
+        if file_name:
+             fp = join(py5.sketch_path(), file_name)
+             with open(fp) as f:
+                 other_src = ''.join(f.read())
+             src += '\n# ' + file_name
+             src += '\n' + other_src 
+    
 
     metadata = PIL.PngImagePlugin.PngInfo()
 
@@ -178,3 +189,50 @@ def save_png_with_src(output=None, *args, **kwargs):
     # read back and print...
     # target_image = PIL.Image.open(output)
     # print(target_image.info['code'])
+    
+def get_arduino(port=None):
+    """
+    This is a PyFirmata 'helper' that tries to connect to
+    an Arduino compatible board.
+    
+    If port is None it tries to connect to the last port
+    listed by pyserial's serial.tools.list_ports.comports().
+    You can provide a string with the port name or an integer
+    index to the port (as listed by pyserial and printed in
+    the console if no port is provided)
+    
+    If successful it returns a pyfirmata Arduino object, but
+    before that it starts a pyfirmata.util.Iterator, and adds
+    to it analog_read() and digital_read() functions that mimic
+    Processing's Firmata library interface (readings are never
+    None, and analog pins return a value between 0 and 1023).
+    """
+    
+    from pyfirmata import Arduino, util
+    from serial.tools import list_ports
+    ports = [comport.device for comport in list_ports.comports()]
+    if not ports:
+        raise Exception('No board/Arduino port found')
+    elif port is None:
+        print('\n'.join(f'{i}: {p}' for i, p in enumerate(ports)))
+        port = len(ports) - 1
+    if isinstance(port, str):
+        print(f'Trying to connect to port: {port}')
+        arduino = Arduino(port)
+    else:
+        print(f'Trying to connect to port {port}: {ports[port]}')
+        arduino = Arduino(ports[port])
+    util.Iterator(arduino).start()
+    for a in range(6):  # A0 A1 A2 A3 A4 A5
+        arduino.analog[a].enable_reporting()
+    arduino.analog_read = (lambda a: round(arduino.analog[a].read() * 1023)
+                           if arduino.analog[a].read() is not None
+                           else 0)
+    digital_pin_dict = {d: arduino.get_pin(f'd:{d}:i')
+                        for d in range(2, 14)}
+    for d in digital_pin_dict.keys():
+        digital_pin_dict[d].enable_reporting()
+    arduino.digital_read = (lambda d: digital_pin_dict[d].read()
+                            if digital_pin_dict[d].read() is not None
+                            else False)
+    return arduino
