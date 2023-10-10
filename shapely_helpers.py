359c2a9d53cb3663d9390f366e46b7f43bc00aaa
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-10-09 21:37:25 -0300

    Update shapely_helpers.py

--- shapely_helpers.py
+++ shapely_helpers.py
@@ -56,14 +56,102 @@ def draw_shapely(shps, sketch: py5.Sketch=None):
         except TypeError as e:
             print(f"Unable to draw: {shps}")
 
+def polys_from_text(words, font, alternate_spacing=False):
+    """
+    Produce a list of shapely Polygons (with holes!) from a string.
+    New-line chars will try to move text to a new line.
+    
+    The alternate_spacing option will pick the glyph
+    spacing from py5.text_width() for each glyph, it can be
+    too spaced, but good for monospaced font alignment.
+    """
+    py5.text_font(font)
+    space_width = py5.text_width(' ')
+    results = []
+    x_offset = y_offset = 0
+    for c in words:
+        if c == '\n':
+            y_offset += font.get_size()
+            x_offset = 0  # assuming left aligned text...
+            continue
+        glyph_pt_lists = [[]]
+        c_shp = font.get_shape(c, 1)
+        vs3 = [c_shp.get_vertex(i) for i in range(c_shp.get_vertex_count())]
+        vs = set()
+        for vx, vy, _ in vs3:
+            x = vx + x_offset
+            y = vy + y_offset
+            glyph_pt_lists[-1].append((x, y))
+            if (x, y) not in vs:
+                vs.add((x, y))
+            else:
+                glyph_pt_lists.append([])  # will leave a trailling empty list
+        
+        if alternate_spacing:
+            w = py5.text_width(c)
+        else:
+            w = c_shp.get_width() if vs3 else space_width
+        x_offset += w
+        # filter out elements with less than 3 points 
+        # and stop before the trailling empty list
+        glyph_polys = [Polygon(p) for p in glyph_pt_lists[:-1] if len(p) > 2]
+        if glyph_polys:  # there are still empty glyphs at this point
+            glyph_shapes = process_glyphs(glyph_polys)
+            results.extend(glyph_shapes)
+    return results
+
+
+def process_glyphs(polys):
+    """
+    Try to subtract the shapely Polygons representing a glyph
+    in order to produce appropriate looking glyphs!
+    """
+    polys = sorted(polys, key=lambda p: p.area, reverse=True)
+    results = [polys[0]]
+    for p in polys[1:]:
+        # works on edge cases like â and ®
+        for i, earlier in enumerate(results):
+            if earlier.contains(p):
+                results[i] = results[i].difference(p)
+                break
+        else:   # the for-loop's else only executes after unbroken loops
+            results.append(p)
+    return results
+
 if __name__ == '__main__':
     def setup():
-        global p
-        py5.size(400, 400)
-        py5.stroke_weight(10)
-        mp = MultiPoint([(200, 200), (100, 100), (200, 300)])
-        draw_shapely(mp)
-        
+        py5.size(800, 800)
+        py5.color_mode(py5.HSB)
+
+        #py5.stroke_weight(10)
+        #mp = MultiPoint([(200, 200), (100, 100), (200, 300)])
+        #draw_shapely(mp)
+
+        t = 'Oi B008!\né o gnumundo®...\n' \
+            'viva a ciberlândia!\n' \
+            '◍◴❂⦲జ్ఞ‌ా'
+      
+        d_font = py5.create_font('SansSerif', 60)
+        i_font = py5.create_font('Open Sans', 60)
+
+        shapes = polys_from_text(
+            t, d_font, alternate_spacing=True)
+
+        for i, shp in enumerate(shapes):
+            py5.fill((i * 8) % 256, 255, 255, 100)
+            draw_shapely(shp)
+
+        shapes = polys_from_text(
+            t, i_font, alternate_spacing=False)
+
+        py5.translate(0, 400)
+
+        for i, shp in enumerate(shapes):
+            py5.fill((i * 8) % 256, 255, 255, 100)
+            draw_shapely(shp)
+
+        py5.translate(100, 100)
+
     py5.run_sketch(block=False)
 
 '''
