005310076200a389eabd4c73bc5f018e291e4ca1
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-04-11 13:33:20 -0300

    Create token_helpers.py

---
+++ token_helpers.py
@@ -0,0 +1,11 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib # tomllib will be in Python 3.11's standard library only
+
+def get_token(category, token):
+
+    with open("/home/villares/api_tokens", "rb") as f:
+        api_tokens = tomllib.load(f)
+
+    return api_tokens[category][token]
